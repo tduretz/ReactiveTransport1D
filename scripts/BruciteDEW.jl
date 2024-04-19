96a976a09eaf9b4ee96c429527bb1f979fdce244
G-Siron <53376789+G-Siron@users.noreply.github.com>
2024-04-19 14:48:14 +0200

    Add activity coefficients for aqueous species
    Add B-dot equation of state for computation of activity coefficients for charged aqueous species and activity coefficients for neutral species

--- scripts/BruciteDEW.jl
+++ scripts/BruciteDEW.jl
@@ -5,19 +5,33 @@ Makie.update_theme!(fonts=(regular=texfont(), bold=texfont(:bold), italic=texfon
 # Comparison with fluid speciation from EQ3 using the DEW thermodynamic database
 # Jacobian constructed with automatic differentiation
 
+"""Compute activity coefficient for fluid species """
+function ActivityCoeff(z, bdot, coeff, å, I, T)
+    logγ = ones(length(z))
+    for i in eachindex(z)
+        if (z[i] == 0)        # Neutral fluid species
+            logγ[i] = ((coeff[1] + coeff[2]*T + coeff[3]/T)*I - (coeff[4] + coeff[5]*T)*(I/(I+1)))/2.303
+        else                  # Charged fluid species
+            logγ[i] = -((bdot[1] * z[i]^2 * I^0.5)/(1 + å[i] * bdot[2] * I^0.5))
+        end
+    end
+
+    return logγ
+end
+
 """ Evaluation of residual """
-function Residual!(f, m, D, b)
+function Residual!(f, m, logγ, D, b)
     for i in eachindex(f)
         if i < 3
             f[i] = D[i, :]' * m - b[i]
         else
-            f[i] = D[i, :]' * log10.(m) - b[i]
+            f[i] = D[i, :]' * log10.(m) + D[i, :]' * logγ - b[i]
         end
     end
 end
 
 """ Optimize globalization parameter of Newton-Raphson solver """
-function LineSearch(f, m, Δm, D, b)
+function LineSearch(f, m, logγ, Δm, D, b)
 
     α  = [0.1 0.25 0.5 0.8 1.0]
     F  = zero(α)
@@ -28,7 +42,7 @@ function LineSearch(f, m, Δm, D, b)
         if minimum(m1) < 0.0 # Do not compute if m<0 otherwise 'Domain Error'
             f .= 1000.0      # In that case set residual to artificially high value (makes sure that it's never identifed as a minimum)
         else
-            Residual!(f, m1, D, b)
+            Residual!(f, m1, logγ, D, b)
         end
         F[i] = norm(f)
     end
@@ -46,6 +60,13 @@ function Speciation()
     df = (CSV.read("data/MatrixBruciteDEW.csv", DataFrame))
     species      = names(df)[2:end] # Read column labels
 
+    # Thermodynamic parameters for activity coefficients
+    Aγ    = 0.6710
+    Bγ    = 0.3595
+    bdot  = [Aγ, Bγ]                                        # B-dot coefficients Aγ and Bγ at 400 C and 5 kbar
+    coeff = [-1.0312, 0.0012806, 255.9, 0.445, -0.001606]   # Coefficients for neutral species C, F, G, E and H
+    å     = [3.7, 3.7, 3.7, 3.7, 3.7, 3.7, 3.7, 3.7]        # Size of fluid species (including hydration shell)
+
     # Parameters
     Clᵗᵒᵗ = 0.01     # Total chlorinity
 
@@ -55,33 +76,40 @@ function Speciation()
     iter  = 0       # Iteration count
 
     # Arrays
-    D  = Float64.(Matrix(df[:, 2:end]))        # Read coefficients and convert to Matrix
-    b  = [0.0; Clᵗᵒᵗ; 6.8466; -1.0841; -0.6078; -8.1764; 6.6296; 4.9398]  
-    n  = length(species)
-    m  = 0.01 * ones(length(b)) # Initial condition
-    f  = zero(m)
-    Δm = zero(m)
-    J  = zeros(n, n)
-    e  = zeros(niter)
+    D    = Float64.(Matrix(df[:, 2:end]))       # Read coefficients and convert to Matrix
+    z    = collect(df[1,2:end])                 # Charges for the fluid species
+    b    = [0.0; Clᵗᵒᵗ; 6.8466; -1.0841; -0.6078; -8.1764; 6.6296; 4.9398]  
+    n    = length(species)
+    m    = 0.01 * ones(length(b))               # Initial condition
+    logγ = ones(length(b))                      # Initial activity coefficients equal to 1
+    f    = zero(m)
+    Δm   = zero(m)
+    J    = zeros(n, n)
+    e    = zeros(niter)
 
     for _ = 1:niter
 
         iter += 1
 
+        # Compute activity coefficients
+        I = (1/2) * sum(m .* z.^2)
+        logγ = ActivityCoeff(z, bdot, coeff, å, I, T)
+        @printf("Ionic strength = %1.6e\n", I)
+
         # Residual evaluation
-        Residual!(f, m, D, b)
+        Residual!(f, m, logγ, D, b)
         e[iter] = norm(f)
-        if norm(f) < 1e-12
+        if norm(f) < ϵ
             break
         end
 
         # Automatic Jacobian generation
-        r = (f, m) -> Residual!(f, m, D, b) # closure
+        r = (f, m) -> Residual!(f, m, logγ, D, b) # closure
         J = ForwardDiff.jacobian(r, f, m)
 
         # Update molalities
         Δm .= -J \ f
-        α   = LineSearch(f, m, Δm, D, b) # Optminise with line search
+        α   = LineSearch(f, m, logγ, Δm, D, b) # Optminise with line search
         m .+= α * Δm
         @printf("it. %03d --- f = %1.4e --- α = %1.2f\n", iter, norm(f), α)
     end
